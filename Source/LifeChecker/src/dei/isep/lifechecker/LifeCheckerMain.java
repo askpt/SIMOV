52b0c6c53743ede28b7af49a7b1c944315d503f0
Tiago Oliveira <tiago.queiros.19@gmail.com>
2014-11-25 22:13:47 +0000

    Teste Git/Eclipse

--- Source/LifeChecker/src/dei/isep/lifechecker/LifeCheckerMain.java
+++ Source/LifeChecker/src/dei/isep/lifechecker/LifeCheckerMain.java
@@ -11,6 +11,7 @@ public class LifeCheckerMain extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
+		
 		setContentView(R.layout.teste);
 		
 		ResponsavelBDD respBDD = new ResponsavelBDD(getApplicationContext());
